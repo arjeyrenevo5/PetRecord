181457ba785a79add865277a2390d6f64153d8bd
tonyooo <talaogonanthony07@gmail.com>
2026-08-29 22:56:03 +0800

    sarap mo jey

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "","");
+$conn = mysqli_connect("localhost", "root", "","ipt2_activity");
 
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
